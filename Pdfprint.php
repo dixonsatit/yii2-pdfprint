ca6d94e2460fe0ac12d6ea772ff115829acc0e7b
Sathit Seethaphon <dixonsatit@gmail.com>
2016-05-25 16:14:09 +0700

    add tag

--- Pdfprint.php
+++ Pdfprint.php
@@ -2,14 +2,51 @@
 
 namespace dixonstarter\pdfprint;
 
+use yii\helpers\Html;
+use yii\helpers\Url;
 /**
  * This is just an example.
  */
 class Pdfprint extends \yii\base\Widget
 {
-    public function run()
-    {
-        
-        return "Hello!";
-    }
+  public $iframeId = 'iframeprint';
+
+  public $elementClass = '.btn-print';
+
+  public function init(){
+    $this->registerJs();
+  }
+
+  public function run()
+  {
+      return Html::tag('iframe',null ,['id' => $this->id, 'style'=>'display:none;']);
+  }
+
+  public function registerJs()
+  {
+$js = <<<JS
+
+$(document).on('click', '{$this->elementClass}', function(e){
+  e.preventDefault();
+  _pdfprint($(this).data('url'));
+});
+
+function _pdfprint(url)
+{
+    var iframeId = "{$this->iframeId}",
+    iframe = $("iframe#{$this->iframeId}");
+    iframe.attr('src', url);
+    iframe.load(function() {
+        _callPdfPrint(iframeId);
+    });
+}
+//initiates print once content has been loaded into iframe
+function _callPdfPrint(iframeId) {
+    var PDF = document.getElementById(iframeId);
+    PDF.focus();
+    PDF.contentWindow.print();
+}
+JS;
+     $this->view->registerJs($js, View::POS_READY, 'dixonstarter-pdfprint');
+ }
 }
